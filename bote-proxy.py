efe528c47c982f05cac35e0c99ff11733c5d265d
Jeff Becker <jeff@i2p.rocks>
2017-09-10 15:04:04 -0400

    more

--- bote-proxy.py
+++ bote-proxy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.6
 
 import email
+from email.message import EmailMessage, MIMEPart
 import smtpd
 import asyncore
 import smtplib
@@ -9,11 +10,47 @@ import configparser
 import traceback
 import sys
 import os
-import sqlalchemy
+import sqlalchemy as SA
+from sqlalchemy.sql import select
 
 unquote = lambda s : s.replace('\'', '').replace('\"', '')
 
 log = lambda s : sys.stdout.write("{}\n".format(s)) and sys.stdout.flush()
+
+class DB:
+
+    def __init__(self, url):
+        self._engine = SA.create_engine(url)
+        self._metadata = SA.MetaData()
+        self._boteusers = SA.Table('boteusers', self._metadata, 
+                                   SA.Column('uid', SA.Integer, primary_key=True, autoincrement=True),
+                                   SA.Column('email', SA.String, nullable=False),
+                                   SA.Column('bote_address', SA.String, nullable=False))
+        
+        self._v_domains = SA.Table('virtual_domains', self._metadata,
+                                   SA.Column('id', SA.Integer, primary_key=True, autoincrement=True),
+                                   SA.Column('name', SA.String(50), nullable=False))
+        
+        self._v_users = SA.Table('virtual_users', self._metadata,
+                                 SA.Column('id', SA.Integer, primary_key=True, autoincrement=True),
+                                 SA.Column('domain_id', SA.Integer, SA.ForeignKey('virtual_domains.id'), nullable=False),
+                                 SA.Column('password', SA.String(106), nullable=False),
+                                 SA.Column('email', SA.String(100), nullable=False))
+
+        self._v_aliases = SA.Table('virtual_aliases', self._metadata,
+                                   SA.Column('id', SA.Integer, primary_key=True, autoincrement=True),
+                                   SA.Column('domain_id', SA.Integer, SA.ForeignKey('virtual_domains.id'), nullable=False),
+                                   SA.Column('source', SA.String(100), nullable=False),
+                                   SA.Column('destination', SA.String(100), nullable=False))
+
+        self._metadata.create_all(self._engine)
+        
+    def getBoteUsers(self, recips):
+        yield from select([self._boteusers.c.bote_address]).where(SA.or_([self._boteusers.c.email == recip for recip in recips])).execute()
+
+    def getLocalusers(self, recips):
+        yield from select([self._v_aliases.c.destination.label('email')]).where(SA.or_([self._v_aliases.c.source == recip for recip in recips])).execute()
+        yield from select([self._v_users.c.email]).where(SA.or_([self._v_users.c.email == recip for recip in recips])).execute()        
 
 class FilterServer(smtpd.SMTPServer):
 
@@ -36,7 +73,7 @@ class FilterServer(smtpd.SMTPServer):
             log(traceback.format_exc())
         else:
             if self.filterMail:
-                msg = self.filterMail(msg)
+                msg = self.filterMail(msg, mailfrom, recips)
             if msg:
                 self.try_inject(mailfrom, recips, msg.as_bytes())
             else:
@@ -55,12 +92,89 @@ class FilterServer(smtpd.SMTPServer):
         except:
             log("try_inject(): {}".format(traceback.format_exc()))
 
+class BoteSender:
 
-def filterMail(msg):
-    """
-    do actual mail filtering
-    """
-    return msg
+    _acceptedMimeTypes = ['multipart/encrypted']
+    _filteringMimeTypes = ['text/plain']
+
+    _bote_host = None
+    _bote_port = None
+    _bote_user = None
+    _bote_passwd = None
+    
+    def __init__(self, db, cfg=None):
+        self._db = db
+        if cfg:
+            self._bote_host = cfg['smtp_host']
+            self._bote_port = cfg['smtp_port']
+            self._bote_user = cfg['smtp_user']
+            self._bote_passwd = cfg['smtp_password']
+    
+    def filterMail(self, msg, mailfrom, recips):
+        """
+        do actual mail filtering
+        """
+        botes = self.getBoteRecips(recips)
+        if botes:
+            localUsers = self.getLocalUsers(recips)
+            newmsg = self.stripMessage(msg)
+            if newmsg:
+                for recip in botes:
+                    self.forwardToBote(recip, newmsg)
+            if localUsers:
+                return msg
+        else:
+            # no bote recipiants
+            return msg
+
+    def getLocalUsers(self, recips):
+        found = list()
+        for recip in self._db.getLocalUsers(recips):
+            found.append(recip[0])
+        if len(found) > 0:
+            return found
+        
+    def getBoteRecips(self, recips):
+        found = list()
+        for recip in self._db.getBoteRecips(recips):
+            found.append(recip[0])
+        if len(found) > 0:
+            return found
+
+
+    def stripMessage(self, msg):
+        """
+        strip email message of anything not encrypted
+        """
+        log("filtering message....")
+        newmsg = EmailMessage()
+        for k in msg.keys():
+            newmsg[k] = msg[k]
+
+        addedParts = 0
+            
+        for part in msg.walk():
+            contentType = part.get_content_type().lower()
+            if contentType in self._acceptedMimeTypes or (contentType in self._filteringMimeTypes and self.partIsEncrypted(part)):
+                newmsg.attach(part)
+                addedParts += 1
+
+        if addedParts > 0:
+            log("attached {} encrypted parts".format(addedParts))
+            return newmsg
+        else:
+            log("no encrypted parts found in message, dropping")
+        
+    def forwardToBote(self, recip, msg):
+        """
+        forward message to i2pbote
+        """
+        if self._bote_host and self._bote_port:
+            log("forwarding bote message to {}:{}".format(self._bote_host, self._bote_port))
+        else:
+            log("no bote forwarding configured, not forwarding to {}".format(recip))
+
+        
 
 def main(args):
     fnames = ["/etc/bote-proxy.ini", "bote-proxy.ini"]
@@ -80,11 +194,22 @@ def main(args):
         log("reading config file {}".format(fname))
         cfg.read(fname)
     except:
-        print("failed to read config file {}: {}".format(fname, traceback.format_exc()))
+        log("failed to read config file {}: {}".format(fname, traceback.format_exc()))
         return
-        
+    if "database" not in cfg:
+        log("no database section in configuration file. Bailing!")
+        return
+    if 'url' not in cfg["database"]:
+        log("no database configured, set url in database section. Bailing!")
+        return
+    
+    log("setting up database...")
+    db = DB(cfg["database"]['url'])
+    log("preparing bote sender...")
+    sender = BoteSender(db)
     log("filter server starting up")
     server = FilterServer(('127.0.0.1', 10025), None)
+    server.filterMail = sender.filterMail
     asyncore.loop()
     
 if __name__ == "__main__":
